e062f30e9a3c23eaf85a0b498fd9328f7b1f2873
Andrewww39 <andrew.sergienko2007@gmail.com>
2025-12-12 18:27:28 +0200

    Part1

---
+++ server.php
@@ -0,0 +1,24 @@
+﻿<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$file = 'menu_data.json';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $json = file_get_contents('php://input');
+    
+    file_put_contents($file, $json);
+    
+    echo json_encode(["status" => "success", "message" => "Меню збережено!"]);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (file_exists($file)) {
+        echo file_get_contents($file);
+    } else {
+        echo json_encode([]);
+    }
+    exit;
+}
+?>
